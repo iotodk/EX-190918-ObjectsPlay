d5e8e111127b1664f19a91b3a37362e2653723d3
Ion Toporan <ioto@eal.dk>
2018-09-19 11:04:48 +0200

    Initial commit

---
+++ ObjectsPlay/Objects/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsPlay.Objects
+{
+    class Car
+    {
+        public string Color;
+        public int NumberOfDoors;
+        public string Brand;
+        public int YearOfFabrication;
+        public int Price;
+        public bool IsUsed;
+
+        public override string ToString()
+        {
+            string text = Brand + " - " + NumberOfDoors + " - " + YearOfFabrication + " - " + Price;
+            return text;
+        }
+        
+    }
+}
---
+++ ObjectsPlay/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ObjectsPlay.Objects;
+
+namespace ObjectsPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the first car
+            Car car1 = new Car();
+            car1.Color = "red";
+            car1.NumberOfDoors = 5;
+            car1.Brand = "Toyota";
+            car1.YearOfFabrication = 2017;
+            car1.Price = 250000;
+            car1.IsUsed = true;
+
+            Car car2 = new Car();
+            car2.Color = "blue";
+            car2.NumberOfDoors = 3;
+            car2.Brand = "BMW";
+            car2.YearOfFabrication = 2000;
+            car2.Price = 1000000;
+            car2.IsUsed = true;
+
+            Car car3 = new Car {
+                Brand = "Opel",
+                Color = "red",
+                YearOfFabrication = 1991,
+                IsUsed = true,
+                NumberOfDoors = 3,
+                Price = 1000
+            };
+
+            Console.WriteLine("Cars for sale:");
+            Console.WriteLine(car1.ToString());
+            Console.WriteLine(car2.ToString());
+            Console.WriteLine(car3.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
